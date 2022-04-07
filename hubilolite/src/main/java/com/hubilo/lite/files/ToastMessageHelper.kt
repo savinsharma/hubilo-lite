b1dfed776cb936616eaea5df650003bb179fe56a
Savin Sharma <savin@hubilo.com>
2022-04-07 17:09:51 +0530

    class to objec

--- hubilolite/src/main/java/com/hubilo/lite/files/ToastMessageHelper.kt
+++ hubilolite/src/main/java/com/hubilo/lite/files/ToastMessageHelper.kt
@@ -3,7 +3,7 @@ package com.hubilo.lite.files
 import android.content.Context
 import android.widget.Toast
 
-open class ToastMessageHelper {
+object ToastMessageHelper {
     fun createToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
